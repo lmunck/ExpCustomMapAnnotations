64e778d6e7a68aedd0e1041aed4333b6dc57770c
lmunck <anders.munck@gmail.com>
2021-04-01 18:06:28 +0200

    Initial Commit

---
+++ ExpCustomMapAnnotations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpCustomMapAnnotations
+//
+//  Created by Anders Munck on 01/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpCustomMapAnnotations/ExpCustomMapAnnotationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpCustomMapAnnotationsApp.swift
+//  ExpCustomMapAnnotations
+//
+//  Created by Anders Munck on 01/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpCustomMapAnnotationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
